6fd9e4179001ef832fa9578504ba64695fc67485
ming <my2153@nyu.edu>
2021-10-17 21:03:09 -0400

    modified entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 cd backend-py/
 source venv/bin/activate
-python -m pip install --upgrade pip
+python3 -m pip install --upgrade pip
 pip install -r requirements.txt
 . entrypoint.sh &
 cd ../frontend-js
